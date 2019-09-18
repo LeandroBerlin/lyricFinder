6bf6b512e47949f0e211b46a47c26a66a16c6834
Leandro <leandro.frigerio@devugees.org>
2019-09-18 11:47:36 +0200

    express API

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
+const fs = require("fs");
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 
 searchString = (req, res) => {
     // Read Synchrously the file
-    const fs = require("fs");
     const content = fs.readFileSync("./lyrics.json");
     // Parse JSON
     const jsonContent = JSON.parse(content);
-
     // The desired length of a text line
     const lineLength = req.params.stringLength ? req.params.stringLength : 23;
 
     // Filter and assign all the lines that match the length
-    const list23 = jsonContent.filter(line => (line['length'] >= lineLength ? line.text : ''))
+    const list23 = jsonContent.filter(line => (line.lengthOfLine >= lineLength ? line.text : ''))
     console.log(list23)
 
     // Random pick one of the lines
-    const randomLine = list23[Math.floor(Math.random() * list23['length'])]
+    const randomLine = list23[Math.floor(Math.random() * list23.length)]
 
     // Console log
     // console.log(`-- This is a line with length ${lineLength} --`)
     // console.log(randomLine.text)
     // console.log(randomLine.type)
-    if (randomLine)
-        res.json({ length: lineLength, string: randomLine.text })
-    else
+
+    //Response
+    randomLine ?
+        res.json({ requested_length: lineLength, string: randomLine.text, length: randomLine.lengthOfLine })
+        :
         res.json({ msg: 'nothing with this length' })
 
 }
 
 
-app.get('/:stringLength?', searchString)
+app.get('/:stringLength?', cors(), searchString)
 
 
 
