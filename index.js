16cb01801ce705c3c0962188018674187b1648e6
Leandro <leandro.frigerio@devugees.org>
2019-09-18 11:34:41 +0200

    express API

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
-// Read Synchrously the file
-const fs = require("fs");
-const content = fs.readFileSync("./lyrics.json");
-// Parse JSON
-const jsonContent = JSON.parse(content);
+const express = require('express')
+const app = express()
 
-// The desired length of a text line
-const lineLength = 23
 
-// Filter and assign all the lines that match the length
-const list23 = jsonContent.filter(line => (line['length'] >= lineLength ? line.text : ''))
-console.log(list23)
+searchString = (req, res) => {
+    // Read Synchrously the file
+    const fs = require("fs");
+    const content = fs.readFileSync("./lyrics.json");
+    // Parse JSON
+    const jsonContent = JSON.parse(content);
 
-// Random pick one of the lines
-const randomLine = list23[Math.floor(Math.random() * list23['length'])]
+    // The desired length of a text line
+    const lineLength = req.params.stringLength ? req.params.stringLength : 23;
 
-// Console log
-console.log(`-- This is a line with length ${lineLength} --`)
-console.log(randomLine.text)
-console.log(randomLine.type)
+    // Filter and assign all the lines that match the length
+    const list23 = jsonContent.filter(line => (line['length'] >= lineLength ? line.text : ''))
+    console.log(list23)
+
+    // Random pick one of the lines
+    const randomLine = list23[Math.floor(Math.random() * list23['length'])]
+
+    // Console log
+    // console.log(`-- This is a line with length ${lineLength} --`)
+    // console.log(randomLine.text)
+    // console.log(randomLine.type)
+    if (randomLine)
+        res.json({ length: lineLength, string: randomLine.text })
+    else
+        res.json({ msg: 'nothing with this length' })
+
+}
+
+
+app.get('/:stringLength?', searchString)
+
+
+
+
+const server = app.listen(3000, () => {
+    console.log('Listening on port %s', server.address().port)
+})
