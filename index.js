45fdaab15630a4bf706648a96b77381c9e521d33
Leandro <leandro.frigerio@devugees.org>
2019-09-19 12:25:29 +0200

    listen port 4000

--- index.js
+++ index.js
@@ -9,59 +9,53 @@ searchString = (req, res) => {
     const content = fs.readFileSync("./lyrics.json");
     // Parse JSON
     const jsonContent = JSON.parse(content);
-    // The desired length of a text line
+    // The desired length of a text line and number of lines
     const lineLengthMin = req.params.stringLengthMin ? req.params.stringLengthMin : 10;
     const lineLengthMax = req.params.stringLengthMax ? req.params.stringLengthMax : 60;
     const numberLines = req.params.numberLines ? req.params.numberLines : 12;
-    // Filter and assign all the lines that match the length + flat to single array
-    const list23 = jsonContent.map(lyric =>
+    // Filter and assign all the lines that match the length + flat the array
+    const listOfLyrics = jsonContent.map(lyric =>
         lyric.filter(line => (line.lengthOfLine >= lineLengthMin && lineLengthMax ? line.text : ''))
     ).flat()
 
+    // Random select n lines
+    const selectLyrics = getRandomLines(listOfLyrics, numberLines)
+    // Check if we have an array otherweise response with error
+    if (selectLyrics instanceof Array) {
+        const lyrics = selectLyrics.map(randomLine => {
+            var lyric = {
+                requested_length: lineLengthMin + ' / ' + lineLengthMax, string: randomLine.text, length: randomLine.lengthOfLine
+            }
+            return lyric;
+        })
 
-    const getRandomLyrics = (arr, n) => {
-        var result = new Array(n),
-            len = arr.length,
-            taken = new Array(len);
-        if (n > len)
-            throw new RangeError("getRandomLyrics: more elements requested than available");
-        while (n--) {
-            var x = Math.floor(Math.random() * len);
-            result[n] = arr[x in taken ? taken[x] : x];
-            taken[x] = --len in taken ? taken[len] : len;
-        }
-        return result;
-    }
-
-    const selectLyrics = getRandomLyrics(list23, numberLines)
-
-    const lyrics = selectLyrics.map(randomLine => {
-
-
-        var lyric = {
-            requested_length: lineLengthMin + ' / ' + lineLengthMax, string: randomLine.text, length: randomLine.lengthOfLine
-        }
-        return lyric;
-    })
-
-    // Console log
-    // console.log(`-- This is a line with length ${lineLength} --`)
-    // console.log(randomLine.text)
-    // console.log(randomLine.type)
-
-    //Response
-    lyrics ?
         res.json(lyrics)
-        :
-        res.json({ msg: 'nothing with this length' })
 
+    } else {
+        res.json({ error: selectLyrics, requested_length: lineLengthMin + ' / ' + lineLengthMax, })
+    }
+    //End of SearchString
+}
+
+// Get random items from an array
+const getRandomLines = (arr, n) => {
+    var result = new Array(n),
+        len = arr.length,
+        taken = new Array(len);
+    if (n > len)
+        return ("No lyrics with the selected parameters");
+    while (n--) {
+        var x = Math.floor(Math.random() * len);
+        result[n] = arr[x in taken ? taken[x] : x];
+        taken[x] = --len in taken ? taken[len] : len;
+    }
+    return result;
 }
 
 
+// Express
+
 app.get('/:stringLengthMin?/:stringLengthMax?/:numberLines?', cors(), searchString)
-
-
-
 
 const server = app.listen(4000, () => {
     console.log('Listening on port %s', server.address().port)
